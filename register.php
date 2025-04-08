c65858611abecd81552304f93c5e6a118299d002
Nass <158141353+DjiwaTsakit@users.noreply.github.com>
2025-04-08 19:38:54 +0700

    Update register.php

--- register.php
+++ register.php
@@ -1,4 +1,8 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php'; // untuk load dotenv
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 require 'vendor/autoload.php';
 require_once 'config/db.php';
 require_once 'config/dotenv.php';
